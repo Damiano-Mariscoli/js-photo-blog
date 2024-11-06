87ad64b1917764a37f5e2f637bd290d361138c72
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-06 17:00:05 +0100

    ADDED: card con il ciclo for

--- js/app.js
+++ js/app.js
@@ -17,30 +17,34 @@
             </div>
           </div>
 */
-const cardHtml = `<div class="col-3">
-            <div class="card debug relative">
-              <div>
-                <img class="absolute pin-center" src="./img/pin.svg" alt="">
-              </div>
-                <div class="card-wrap d-flex justify-center flex-column align-center p-4 gap-5">
-                    <div>
-                        <img class="image" src="https://via.placeholder.com/600/92c952" alt="">
-                    </div>
-                    <div class="caption">
-                        <p>
-                           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora atque unde non possimus error blanditiis!
-                        </p>
-                    </div>
-                </div>
-            </div>
-          </div>`
 
+
+card = document.getElementById('row-card')
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
   .then(el => {
     const data = el.data;
+    console.log(data)
     data.forEach(photo => {
-     
+      card.innerHTML += `
+            <div class="col-3">
+              <div class="card debug relative">
+                <div>
+                  <img class="absolute pin-center" src="./img/pin.svg" alt="">
+                </div>
+                <div class="card-wrap d-flex justify-center flex-column align-center p-4 gap-5">
+                  <div>
+                    <img class="image" src="${photo.url}" alt="">
+                  </div>
+                  <div class="caption">
+                    <p>
+                      ${photo.title}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+      `
     })
   })
   .catch(err => console.error(err));
