f755ec3f8bff9ba7406ae705627c482be6e0631c
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-09 04:18:58 +0100

    ADDED: versione aggiornata, metodo di creazione card implementato con mycreateElement

--- js/app.js
+++ js/app.js
@@ -1,81 +1,117 @@
 /*
 <div class="col-3">
-            <div class="card debug relative">
-              <div>
-                <img class="absolute pin-center" src="./img/pin.svg" alt="">
-              </div>
-                <div class="card-wrap d-flex justify-center flex-column align-center p-4 gap-5">
-                    <div>
-                        <img class="image" src="https://via.placeholder.com/600/92c952" alt="">
-                    </div>
-                    <div class="caption">
-                        <p>
-                           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora atque unde non possimus error blanditiis!
-                        </p>
-                    </div>
-                </div>
+    <div class="card debug relative">
+        <div>
+            <img class="absolute pin-center" src="./img/pin.svg" alt="">
+        </div>
+        <div class="card-wrap d-flex justify-center flex-column align-center p-4 gap-5">
+            <div>
+                <img class="image" src="https://via.placeholder.com/600/92c952" alt="">
             </div>
-          </div>
+            <div class="caption">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora atque unde non possimus error blanditiis!
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 */
 
-card = document.getElementById("row-card");
-console.log(card);
-axios
-  .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-  .then((el) => {
-    const data = el.data;
-    console.log(el);
-    console.log(data);
-    data.forEach((photo) => {
-      const cardElement = document.createElement("div");
-      cardElement.className = "col-3";
-      cardElement.innerHTML = `
-            <div class="card relative">
-              <div>
-                <img class="absolute pin-center" src="./img/pin.svg" alt="">
-              </div>
-                <div class="card-wrap d-flex justify-center flex-column align-center p-4 gap-5">
-                    <div>
-                        <img class="image" src="${photo.url}" alt="">
-                    </div>
-                    <div class="caption">
-                        <p>
-                           ${photo.title}
-                        </p>
-                    </div>
-                </div>
-            </div>
-          </div>
-      `;
-      card.appendChild(cardElement);
-      cardElement.addEventListener("click", (e) => {
-        console.log(`${photo.id}`);
-        const overlayElement = document.createElement("div");
-        overlayElement.className =
-          "overlay d-flex justify-center align-center flex-column gap-5 relative";
-        overlayElement.innerHTML = `
-          <img class="img-overlay" src="${photo.url}" alt="">
-          <button id="btn" class="absolute btn">chiudi</button>
-            
-        `;
-        document.body.appendChild(overlayElement);
+const imgOverlay = document.getElementById("imgOverlay");
+const overlay = document.getElementById("overlay");
+let cardElement;
+const button = document.getElementById("button");
+const cardContainer = document.getElementById("row-card");
+console.log(cardContainer);
 
-        
+function myCreateElement4(
+  tagnName,
+  classList = [],
+  content = [],
+  callback = false
+) {
+  const el = document.createElement(tagnName);
 
-        button = document.getElementById("btn");
-        button.addEventListener("click", (e) => {
-          e.stopPropagation();
-          console.log("chiudi con btn");
-          overlayElement.remove(overlayElement);
-          
-        });
-        overlayElement.addEventListener("click", (e) => {
-          console.log("chiudi con overlay");
-          if (e.target.tagName !== 'IMG')     
-          overlayElement.remove(overlayElement);
-        });
-      });
+  if (classList.length > 0) {
+    el.classList.add(...classList);
+  }
+
+  if (callback) {
+    callback(el);
+  }
+
+  if (Array.isArray(content)) {
+    for (let i = 0; i < content.length; i++) {
+      el.appendChild(content[i]);
+    }
+  } else if (content instanceof HTMLElement) {
+    el.appendChild(content);
+  } else if (typeof content === "string") {
+    el.innerHTML = content;
+  } else {
+    console.error("Non posso aggiungere l'elemento");
+  }
+
+  return el;
+}
+
+function toggleOverlay() {
+  overlay.classList.toggle("d-none");
+  document.body.classList.toggle("overflow-hidden");
+}
+
+axios.get("https://picsum.photos/v2/list?page=12&limit=6").then((el) => {
+  const data = el.data;
+  console.log(el);
+  console.log(data);
+  console.log;
+
+  const cardElements = data.map((photo) => {
+    return myCreateElement4("div", ["col-3"], [
+      myCreateElement4("div", ["card", "relative"], [
+        myCreateElement4("div", [], [
+          myCreateElement4("img", ["absolute", "pin-center"], "", (el) => {
+            el.src = "./img/pin.svg";
+            el.alt = "pin";
+          })
+        ]),
+        myCreateElement4("div", ["card-wrap", "d-flex", "justify-center", "flex-column", "align-center", "p-4", "gap-5"], [
+          myCreateElement4("div", [], [
+            myCreateElement4("img", ["image"], "", (el) => {
+              el.src = `${photo.download_url}`;
+              el.alt = "";
+            })
+          ]),
+          myCreateElement4("div", ["caption"], [
+            myCreateElement4("p", [], `${photo.author}`)
+          ])
+        ])
+      ])
+    ]);
+  });
+
+  cardElements.forEach((card, i) => {
+    console.log(i)
+    console.log(data[i].download_url);
+    cardContainer.appendChild(card);
+    card.addEventListener("click", () => {
+      toggleOverlay();
+      imgOverlay.src = data[i].download_url;
     });
-  })
+  });
 
-  .catch((err) => console.error(err));
+  overlay.addEventListener("click", (e) => {
+    if (e.target.tagName !== "IMG") {
+      toggleOverlay();
+      console.log("chiudi con overlay");
+    } else console.log("click sull'immagine");
+  });
+
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleOverlay();
+    console.log("chiudi con bottone");
+  });
+
+});
